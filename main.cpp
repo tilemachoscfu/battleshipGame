5f2f1a380ad6ac8874b374b958e9b027e9b5293a
Tilemachos Zoitsas <tilemahoscfu@gmail.com>
2017-06-01 00:34:16 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,11 +18,11 @@ int grid[rows][columns];
 // Initialize
 void initArray()
 {
-    for(int i = 0; i < rows; i++)    //This loops on the rows.
+        for(int i = 0; i < rows; i++) //This loops on the rows.
         {
-            for(int j = 0; j < columns; j++) //This loops on the columns
+                for(int j = 0; j < columns; j++) //This loops on the columns
                 {
-                    grid[i][j] = SEA;
+                        grid[i][j] = SEA;
                 }
         }
 }
@@ -30,41 +30,41 @@ void initArray()
 // Display the board
 void printArray()
 {
-    cout << setw(4) << " ";
-    cout << "0 1 2 3 4 5 6 7 8 9" << endl;
-    for(int i = 0; i < rows; i++)
+        cout << setw(4) << " ";
+        cout << "0 1 2 3 4 5 6 7 8 9" << endl;
+        for(int i = 0; i < rows; i++)
         {
-        cout << setw(2) << i << "  ";
-            for(int j = 0; j < columns; j++)
+                cout << setw(2) << i << "  ";
+                for(int j = 0; j < columns; j++)
                 {
-                    if(grid[i][j] == SHIP || grid[i][j] == SEA)
-                    {
-                        cout << "* ";
-                    }
-                    else if(grid[i][j] == NOHIT)
-                    {
-                        cout << "O ";
-                    }
-                    else if(grid[i][j] == HIT)
-                    {
-                        cout << "X ";
-                    }
+                        if(grid[i][j] == SHIP || grid[i][j] == SEA)
+                        {
+                                cout << "* ";
+                        }
+                        else if(grid[i][j] == NOHIT)
+                        {
+                                cout << "O ";
+                        }
+                        else if(grid[i][j] == HIT)
+                        {
+                                cout << "X ";
+                        }
                 }
-            cout << endl;
+                cout << endl;
         }
 }
 
 // Number of ships
 int numOfShips()
 {
-    int c = 0;
+        int c = 0;
 
-    for(int i = 0; i < rows; i++)    //This loops on the rows.
+        for(int i = 0; i < rows; i++) //This loops on the rows.
         {
-            for(int j = 0; j < columns; j++) //This loops on the columns
+                for(int j = 0; j < columns; j++) //This loops on the columns
                 {
-                    if(grid[i][j] == SHIP)
-                    c++;
+                        if(grid[i][j] == SHIP)
+                                c++;
                 }
         }
         return c;
@@ -75,76 +75,74 @@ int numOfShips()
 //Generate random ships
 void gnrShips()
 {
-    int s = 0;
-    while(s < ships)
-    {
-        int x = rand() % rows;
-        int y = rand() % columns;
-        if(grid[x][y] != 1)
+        int s = 0;
+        while(s < ships)
         {
-            s++;
-            grid[x][y] = SHIP;
+                int x = rand() % rows;
+                int y = rand() % columns;
+                if(grid[x][y] != 1)
+                {
+                        s++;
+                        grid[x][y] = SHIP;
 
+                }
         }
-    }
 }
 
 bool userAttack(int x, int y)
 {
-    if(grid[x][y] == SHIP)
-    {
-        grid[x][y] = HIT;
-        return true;
-    }
-    else if(grid[x][y] = NOHIT)
-    {
-        return false;
-    }
+        if(grid[x][y] == SHIP)
+        {
+                grid[x][y] = HIT;
+                return true;
+        }
+        else if(grid[x][y] = NOHIT)
+                return false;
 }
 
 
 int main()
 {
-    srand(time(0));
-    initArray();
-    printArray();
-    gnrShips();
-    int position1, position2;
-    int trials = 20;
+        srand(time(0));
+        initArray();
+        printArray();
+        gnrShips();
+        int position1, position2;
+        int trials = 20;
 
-    cout << "---------------------------------" << endl;
-    cout << "Object of Battleship:\nTo become the winner of Battleship you must be able to find (sink)\nall ten ships in your opponent’s fleet" << endl;
-    cout << "---------------------------------" << endl;
-    do
-    {
+        cout << "---------------------------------" << endl;
+        cout << "Object of Battleship:\nTo become the winner of Battleship you must be able to find (sink)\nall ten ships in your opponent’s fleet" << endl;
+        cout << "---------------------------------" << endl;
+        do
+        {
 
-        cout << endl << "Please input location(ROW,COLUMN): "<<  endl;
-        cin >> position1 >> position2;
-        bool found = false;
+                cout << endl << "Please input location(ROW,COLUMN): "<<  endl;
+                cin >> position1 >> position2;
+                bool found = false;
 
-        if(userAttack(position1,position2)){
-            found = true;
-            cout << endl << "---------------------------------" << endl;
-            cout << endl << "There is a ship at that Position." <<  endl;
-            cout << endl << "---------------------------------" << endl;
+                if(userAttack(position1,position2)) {
+                        found = true;
+                        cout << endl << "---------------------------------" << endl;
+                        cout << endl << "There is a ship at that Position." <<  endl;
+                        cout << endl << "---------------------------------" << endl;
 
                 }
-        if(!found)
-        {
-        trials--;
-        cout << endl << "------------------------" << endl;
-        cout << "Sorry no ship at that Position" << endl;
-        cout << "------------------------" << endl;
-        cout << "Number of ships left: " << numOfShips() << endl;
-        cout << "Number of tries left: " << trials << endl;
-        cout << "------------------------" << endl;
-        }printArray();
-    }while(trials > 0);
-    cout << endl << "---------------------------------" << endl;
-    cout << endl << "Game Over!" << endl;
-    cout << endl << "---------------------------------" << endl;
+                if(!found)
+                {
+                        trials--;
+                        cout << endl << "------------------------" << endl;
+                        cout << "Sorry no ship at that Position" << endl;
+                        cout << "------------------------" << endl;
+                        cout << "Number of ships left: " << numOfShips() << endl;
+                        cout << "Number of tries left: " << trials << endl;
+                        cout << "------------------------" << endl;
+                } printArray();
+        } while(trials > 0);
+        cout << endl << "---------------------------------" << endl;
+        cout << endl << "Game Over!" << endl;
+        cout << endl << "---------------------------------" << endl;
 
-    printArray();
-    return 0;
+        printArray();
+        return 0;
 
 }
